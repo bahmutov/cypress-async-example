676622fb24f01c80d8c6a337f52a12b43cd29e0a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-06 22:35:54 -0500

    more refactoring

---
+++ cypress/e2e/add-todos4.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+// refactoring async code
+async function removeUser(user) {
+  const todos = await (await fetch(`/todos/?userId=${user.id}`)).json()
+  // delete each todo
+  for (const todo of todos) {
+    await fetch(`/todos/${todo.id}`, { method: 'DELETE' })
+  }
+  // delete the user
+  await fetch(`/users/${user.id}`, { method: 'DELETE' })
+}
+
+beforeEach(() => {
+  // get all users
+  cy.request('/users').its('body').each(removeUser)
+})
+
+it('adds todos for the user', () => {
+  cy.visit('/')
+  cy.get('#users tbody[loaded=true]')
+  cy.get('#new-user').type('Joe{enter}').type('Anna{enter}').type('Mary{enter}')
+  cy.get('table#users tbody tr').should('have.length', 3)
+  cy.contains('#users tr', 'Anna').find('input[type=radio]').click()
+  cy.get('input#new-todo').should('be.visible').type(`one{enter}`)
+  cy.get('input#new-todo').should('have.value', '')
+  // add two more todos
+  cy.get('input#new-todo').type('second{enter}')
+  cy.get('input#new-todo').type('third{enter}')
+  cy.get('table#todos tbody tr').should('have.length', 3)
+})
